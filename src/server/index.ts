9fd5fd9256b2657ff10db1db991723444873d9a7
Kate Miranda <kate.miranda@whitecobalt.com>
2023-11-14 16:13:59 +0800

    fix sqlite db path

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,7 +1,8 @@
 import { drizzle } from "drizzle-orm/better-sqlite3"
 import Database from "better-sqlite3"
 import * as schema from "./schema"
+import path from "path"
 
-const sqliteConnection = new Database("sqlite.db")
+const sqliteConnection = new Database(path.resolve(process.cwd(), "./sqlite.db"))
 
 export const db = drizzle(sqliteConnection, { schema })

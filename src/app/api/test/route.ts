f0c8c63be0f145ec27429f90f1661a4bf50f353b
Kate Miranda <kate.miranda@whitecobalt.com>
2023-11-14 15:46:49 +0800

    test error sqlite db

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,10 +1,25 @@
+import Database from "better-sqlite3"
+import { drizzle } from "drizzle-orm/better-sqlite3"
 import { NextRequest } from "next/server"
+import * as schema from "@/server/schema"
 
 export async function GET(request: NextRequest) {
     const searchParams = request.nextUrl.searchParams
     const query = searchParams.get('q')
+    try {
 
-    return Response.json({
-        query
-    })
+        const sqliteConnection = new Database("sqlite.db")
+    
+         const db = drizzle(sqliteConnection, { schema })
+
+         return Response.json({
+            query
+        })
+    } catch (error: any) {
+        return Response.json({
+            query: error.message
+        }) 
+    }
+
+    
 }
